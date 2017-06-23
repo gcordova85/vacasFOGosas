62265ec30b34b834fe9fc8f37fe283820911601b
gcordova85 <gcordova85@gmail.com>
2017-06-23 06:31:47 -0300

    Orden del repo
    se dejan en la raiz los archivos de codigo fuente que se estan
    utilizando, comienza la limpieza de codigo

--- vmain.py
+++ vmain.py
@@ -1,21 +1,17 @@
-import threading
-import vclases
+from vclases import *
 from threading import Thread, Lock
 from vprocesos import *
-
-
-
 
 def constructor():
 
 	v1=threading.Thread(target=vacaVive, args=("Vaca1",))
 	v1.start()
 
-	# v2=threading.Thread(target=vacaVive, args=("vaca2",))
-	# v2.start()
+	v2=threading.Thread(target=vacaVive, args=("vaca2",))
+	v2.start()
 
-	# v3=threading.Thread(target=vacaVive, args=("vaca3",))
-	# v3.start()
+	v3=threading.Thread(target=vacaVive, args=("vaca3",))
+	v3.start()
 	
 constructor()
 
